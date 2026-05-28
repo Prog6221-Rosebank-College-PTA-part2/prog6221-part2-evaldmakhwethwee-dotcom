fe2d62f26f5cd1d37c08ae8c6b40cd04b238ba28
evaldmakhwethwee-dotcom <evaldmakhwethwee@gmail.com>
2026-05-28 16:30:00 +0200

    created a chatbot with textbox, listbox, button and label

--- WinFormsApp2/WinFormsApp2/Form1.Designer.cs
+++ WinFormsApp2/WinFormsApp2/Form1.Designer.cs
@@ -28,12 +28,67 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            label1 = new Label();
+            button1 = new Button();
+            listBox1 = new ListBox();
+            textBox1 = new TextBox();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 21F);
+            label1.Location = new Point(318, 33);
+            label1.Name = "label1";
+            label1.Size = new Size(146, 47);
+            label1.TabIndex = 0;
+            label1.Text = "chatapp";
+            // 
+            // button1
+            // 
+            button1.Location = new Point(498, 349);
+            button1.Name = "button1";
+            button1.Size = new Size(94, 29);
+            button1.TabIndex = 1;
+            button1.Text = "Send";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // listBox1
+            // 
+            listBox1.FormattingEnabled = true;
+            listBox1.Location = new Point(223, 112);
+            listBox1.Name = "listBox1";
+            listBox1.Size = new Size(415, 204);
+            listBox1.TabIndex = 2;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(246, 351);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(205, 27);
+            textBox1.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(textBox1);
+            Controls.Add(listBox1);
+            Controls.Add(button1);
+            Controls.Add(label1);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Label label1;
+        private Button button1;
+        private ListBox listBox1;
+        private TextBox textBox1;
     }
 }
